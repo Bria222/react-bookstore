76aa188dbb2d60c2706a0b96b4bf48a688878234
Bria222 <briantechologies.co.ke@gmail.com>
2022-06-10 03:38:34 +0300

    fix linters

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,38 +3,38 @@ import { useSelector, useDispatch } from 'react-redux';
 import { deleteBook } from '../Redux/books/books';
 
 function Book() {
-	const showBook = useSelector((state) => state.books);
-	const dispatch = useDispatch();
+  const showBook = useSelector((state) => state.books);
+  const dispatch = useDispatch();
 
-	const deleteHandler = (id) => {
-		dispatch(deleteBook(id));
-	};
+  const deleteHandler = (id) => {
+    dispatch(deleteBook(id));
+  };
 
-	return (
-  <li className="bookList">
-    <div className="bookContainer">
-      <span>
-        {showBook.map((book) => (
-          <div key={book.id} className="eachbook">
-            {book.title}
-            {' '}
-            :
-            {book.author}
-            <button
-              type="button"
-              className="delete"
-              onClick={() => {
-									deleteHandler(book.id);
-								}}
-            >
-              Delete
-            </button>
-          </div>
-					))}
-      </span>
-    </div>
-  </li>
-	);
+  return (
+    <li className="bookList">
+      <div className="bookContainer">
+        <span>
+          {showBook.map((book) => (
+            <div key={book.id} className="eachbook">
+              {book.title}
+              {' '}
+              :
+              {book.author}
+              <button
+                type="button"
+                className="delete"
+                onClick={() => {
+                  deleteHandler(book.id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          ))}
+        </span>
+      </div>
+    </li>
+  );
 }
 
 export default Book;
